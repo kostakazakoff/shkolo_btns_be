19c2eb974ab570765b159780eb56637b94634703
Kosta Kazakov <kostakazakoff@gmail.com>
2024-11-23 04:36:47 +0200

    rollback app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->statefulApi();
         $middleware->validateCsrfTokens(except: [
             'api/*',
         ]);
